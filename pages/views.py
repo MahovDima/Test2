34199492536e7e5369fd835bcb392b1071b7cb26
petrov <cassa3000@gmail.com>
2022-09-06 12:50:45 +0300

    Create

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "home.html")
+
+def index2(request):
+    return render(request, "about.html")
